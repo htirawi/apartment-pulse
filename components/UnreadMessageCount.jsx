ee3720eade94c43fe6a53e35d55656f8877318f5
Hussein Tirawi <hmtirawi@gmail.com>
2024-04-23 23:47:13 +0300

    added some fixes to build

--- components/UnreadMessageCount.jsx
+++ components/UnreadMessageCount.jsx
@@ -21,7 +21,7 @@ const UnreadMessageCount = ({ session }) => {
       }
     };
     fetchUnreadMessages();
-  }, [session]);
+  }, [session, setUnreadCount]);
 
   return unreadCount > 0 ? (
     <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
