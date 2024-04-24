63388f5b351eaf42d2572584886380c0d1a3c1d2
Hussein Tirawi <32430109+htirawi@users.noreply.github.com>
2024-04-24 12:34:05 +0300

    fix build issues (#20)

--- utils/requests.js
+++ utils/requests.js
@@ -2,43 +2,33 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch apartments from the API
 async function fetchApartments({ showFeatured = false } = {}) {
-  try {
-    // handle if domain is not available
-    if (!apiDomain) {
-      return [];
-    }
-    const response = await fetch(
-      `${apiDomain}/apartments${showFeatured ? '/featured' : ''}`,
-      { next: { revalidate: 0 } }
-    );
-    if (!response.ok) {
-      throw new Error('Failed to fetch apartments');
-    }
-    return response.json();
-  } catch (error) {
-    console.error(error);
+  // handle if domain is not available
+  if (!apiDomain) {
     return [];
   }
+  const response = await fetch(
+    `${apiDomain}/apartments${showFeatured ? '/featured' : ''}`,
+    { cache: 'no-store' }
+  );
+  if (!response.ok) {
+    throw new Error('Failed to fetch apartments');
+  }
+  return response.json();
 }
 
 // Fetch a single apartment from the API
 async function fetchApartment(id) {
-  try {
-    // handle if domain is not available
-    if (!apiDomain) {
-      return null;
-    }
-    const response = await fetch(`${apiDomain}/apartments/${id}`);
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch apartment');
-    }
-
-    return response.json();
-  } catch (error) {
-    console.error(error);
-    return null;
+  // handle if domain is not available
+  if (!apiDomain) {
+    return [];
   }
+  const response = await fetch(`${apiDomain}/apartments/${id}`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch apartment');
+  }
+
+  return response.json();
 }
 
 export { fetchApartments, fetchApartment };
